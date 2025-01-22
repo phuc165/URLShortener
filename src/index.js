795f9bb55ddfec5000c37b24a71e7fb861f603a3
phuc165 <hatanphuc3456789@gmail.com>
2025-01-22 21:56:54 +0700

    code formatter testing

--- src/index.js
+++ src/index.js
@@ -1,32 +1,35 @@
 import express from 'express';
 import { engine } from 'express-handlebars';
 import morgan from 'morgan';
-const app = express()
-const port = 3000
+const app = express();
+const port = 3000;
 
 import route from './routes/index.js';
 
 app.use(express.static('src/public'));
 
 //middleware
-app.use(express.urlencoded(
-  { extended: true }
-));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 //HTTP request logger
-app.use(morgan('combined'))
+app.use(morgan('combined'));
 
 //template engine
-app.engine('.hbs', engine({
-  extname: '.hbs'
-}));
+app.engine(
+    '.hbs',
+    engine({
+        extname: '.hbs',
+    }),
+);
 app.set('view engine', 'hbs');
 app.set('views', './src/resource/views');
 
 //routes init
 route(app);
 
+//this is a test.....
+
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+    console.log(`Example app listening on port ${port}`);
+});
