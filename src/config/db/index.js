5d589a2463477e7408908858fbd647c6c85c3f5a
phuc165 <hatanphuc3456789@gmail.com>
2025-01-24 12:44:02 +0700

    adding mongodb atlas

--- src/config/db/index.js
+++ src/config/db/index.js
@@ -2,7 +2,9 @@ import mongoose from 'mongoose';
 
 async function connect() {
     try {
-        await mongoose.connect('mongodb://127.0.0.1:27017/url_shortener');
+        await mongoose.connect(
+            'mongodb+srv://hatanphuc3456789:<db_password>@urlshortener.86mqk.mongodb.net/?retryWrites=true&w=majority&appName=URLShortener',
+        );
         console.log('Connected to MongoDB');
     } catch (error) {
         console.error('Error connecting to MongoDB', error);
